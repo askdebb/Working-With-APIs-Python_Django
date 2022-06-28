072aef4ff61c8f7375fb914a090a3b8b8ee114c2
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-06-28 02:52:46 +0000

    in progress with modifications

--- I4G014113TWF/links/models.py
+++ I4G014113TWF/links/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+who = get_user_model()
+
+class Link(models.Model):
+    target_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(max_length=20,blank=True, unique=True)
+    author = models.ForeignKey(who,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.description
+
+
